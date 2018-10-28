e93c822b5462b84d26cebcdfb183a6a79939faff
Kyle De Sousa <kyledesousa@finestdental.co.uk>
2018-10-28 09:14:42 +0000

    campaign changes

--- src/js/app.js
+++ src/js/app.js
@@ -312,44 +312,44 @@ $(document).ready(function() {
     // }
 
     // defer FB pixel
-    // var dfbp = setTimeout(function(){
-    //     !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    //         n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-    //         n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-    //         t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-    //         document,'script','https://connect.facebook.net/en_US/fbevents.js');
-    //     fbq('init', '1477526785873521'); // Insert your pixel ID here.
-    //     fbq('track', 'PageView');
-    // },3000);
+    var dfbp = setTimeout(function(){
+        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+            document,'script','https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1477526785873521'); // Insert your pixel ID here.
+        fbq('track', 'PageView');
+    },3000);
 
     // defer GA loading
-    // var dlga = setTimeout(function(){
-    //
-    //     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    //         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    //         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    //     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    //
-    //     ga('create', 'UA-58568623-1', 'auto');
-    //     ga('send', 'pageview');
-    // },1200);
+    var dlga = setTimeout(function(){
+
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-58568623-1', 'auto');
+        ga('send', 'pageview');
+    },1200);
 
     // defer ResponseTap loading
-    // var dlrt = setTimeout(function(){
-    //     var headID = document.getElementsByTagName("head")[0];
-    //     adiInit = "35441";
-    //     adiRVO = true;
-    //     adiFunc = null;
-    //     // (function() {
-    //     adiSrc = document.createElement("script"); adiSrc.type = "text/javascript";
-    //     adiSrc.async = true;
-    //     adiSrc.src = ("https:" == document.location.protocol ? "https://static-ssl" : "http://static-cdn")+ ".responsetap.com/static/scripts/rTapTrack.min.js";
-    //
-    //     // var s = document.getElementsByTagName("script")[0];
-    //     // s.parentNode.insertBefore(adiSrc, s);
-    //     headID.appendChild(adiSrc);
-    //     // })();
-    // },500);
+    var dlrt = setTimeout(function(){
+        var headID = document.getElementsByTagName("head")[0];
+        adiInit = "35441";
+        adiRVO = true;
+        adiFunc = null;
+        // (function() {
+        adiSrc = document.createElement("script"); adiSrc.type = "text/javascript";
+        adiSrc.async = true;
+        adiSrc.src = ("https:" == document.location.protocol ? "https://static-ssl" : "http://static-cdn")+ ".responsetap.com/static/scripts/rTapTrack.min.js";
+
+        // var s = document.getElementsByTagName("script")[0];
+        // s.parentNode.insertBefore(adiSrc, s);
+        headID.appendChild(adiSrc);
+        // })();
+    },500);
 
     // delayed load of above-the-fold reviews widget
     var dr = setTimeout(function() {

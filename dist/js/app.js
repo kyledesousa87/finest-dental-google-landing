4315ee9095eeb0655f7eeaff307c457373fddb31
Kyle De Sousa <kyledesousa@finestdental.co.uk>
2018-11-08 22:16:29 +0000

    updated files

--- dist/js/app.js
+++ dist/js/app.js
@@ -3876,17 +3876,7 @@ $(document).ready(function() {
         fbq('track', 'PageView');
     },3000);
 
-    // defer GA loading
-    var dlga = setTimeout(function(){
 
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-58568623-1', 'auto');
-        ga('send', 'pageview');
-    },1200);
 
 
     // delayed load of above-the-fold reviews widget
